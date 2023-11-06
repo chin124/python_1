b24219564ae11ac1c1ab9428a21599c49ae17f54
chin124 <112843803+chin124@users.noreply.github.com>
2023-11-06 17:05:16 +0530

    Add files via upload

---
+++ Example3.py
@@ -0,0 +1,11 @@
+
+import shutil
+
+#  Python allows you to quickly create zip/tar archives.
+# Following command will zip entire directory
+
+# here "myzip.zip" will be created of the current working directory (. is given) and that zip file will be stored in the current working directory itself.
+
+shutil.make_archive("myzip.zip","zip",".")
+print("Done")
+
